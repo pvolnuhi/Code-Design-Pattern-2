0872739a5888e3f4216fd6e6c7fe9980c5e4a5f3
Polina Volnuhina <55810259+pvolnuhi@users.noreply.github.com>
2021-06-10 20:31:53 -0700

    Update CoffeeMaker.java

--- src/lab/assignment/beforerefactoring/CoffeeMaker.java
+++ src/lab/assignment/beforerefactoring/CoffeeMaker.java
@@ -1,48 +1,35 @@
 package lab.assignment.beforerefactoring;
 
 public class CoffeeMaker {
-
+//client does not change!
+//make class
     public void prepareMochaCoffee(){
         boilWater();
         brewEspresso();
-        addIngredientsForMocha();
-        finalTouchForMocha();
+        addIngredients();
+        finalTouch();
         System.out.println(">>Serving coffee");
     }
 
-    public void prepareCapuccinoCoffee(){
+    public final void prepareCoffee(){
         boilWater();
         brewEspresso();
-        addIngredientsForCapuccino();
-        finalTouchForCapuccino();
+        addIngredients();
         System.out.println(">>Serving coffee");
+
     }
 
+    //default implementation
     private void boilWater() {
         System.out.println("Boiling water");
     }
 
+    //methods to be implemented by subclasses
+    protected abstract void addIngredients();
+    protected abstract void finalTouch();
+
     private void brewEspresso() {
         System.out.println("Brewing espresso");
     }
-
-    private void addIngredientsForMocha(){
-        System.out.println("Adding cocoa powder");
-        System.out.println("Adding hot milk");
-    }
-
-    private void addIngredientsForCapuccino(){
-        System.out.println("Add hot milk");
-        System.out.println("Add cinnamon");
-    }
-
-    private void finalTouchForMocha(){
-        System.out.println("Adding sugar");
-    }
-
-    private void finalTouchForCapuccino(){
-        System.out.println("Top with milk foam");
-    }
-
 
 }
