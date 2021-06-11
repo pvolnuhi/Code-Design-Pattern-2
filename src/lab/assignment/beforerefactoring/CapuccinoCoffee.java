a6699ae18507a1c2833842e8601ddfee71288bcc
Polina Volnuhina <55810259+pvolnuhi@users.noreply.github.com>
2021-06-10 20:31:31 -0700

    Update CapuccinoCoffee.java

--- src/lab/assignment/beforerefactoring/CapuccinoCoffee.java
+++ src/lab/assignment/beforerefactoring/CapuccinoCoffee.java
@@ -2,10 +2,10 @@ package lab.example.afterrefactoring;
 
 public class CapuccinoCoffee extends CoffeeMaker{
 
-	@Override
-	protected void addIngredients(){
-        System.out.println("Add hot milk");
-        System.out.println("Add cinnamon");
+    @Override
+    protected void addIngredients(){
+    	System.out.println("Add hot milk");
+    	System.out.println("Add cinnamon");
     }
 
     @Override
