0f6ece61ae3e1f5ff61b70f46f0f7dde3377411d
Polina Vyecheslav Volnuhina <pvolnuhi@unix4.csc.calpoly.edu>
2021-06-10 20:26:02 -0700

    Fix bad repo

---
+++ src/lab/assignment/beforerefactoring/CapuccinoCoffee.java
@@ -0,0 +1,15 @@
+package lab.example.afterrefactoring;
+
+public class CapuccinoCoffee extends CoffeeMaker{
+
+	@Override
+	protected void addIngredients(){
+        System.out.println("Add hot milk");
+        System.out.println("Add cinnamon");
+    }
+
+    @Override
+    protected void finalTouch(){
+        System.out.println("Top with milk foam");
+    }
+}
